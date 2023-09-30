86d16d3f96ad62a297a2640621c0ca93a5d7af0b
Vadim <skinnoniks@gmail.com>
2023-09-30 11:56:51 +0300

    Add  Jquery  (#2)
    * Add index.html
    
    * Fix indentation
    
    * Add gitignore
    
    * Add server.js
    
    * Add server.js dependencies
    
    * Add assets
    
    * Add articles to index.html
    
    * Add form styles
    
    * Add arrow animation
    
    * Add subscribe form
    
    * Add form an arrow animation
    
    * Add go to top button
    
    * Add slide and body fadein animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,22 @@
-var imgObj = null;
-            
-function init() {
-   imgObj = document.getElementById('arrow');
-   imgObj.style.position= 'relative'; 
-   imgObj.style.left = '0px'; 
-}
 function moveRight() {
-   imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+    const left = $('#arrow').css('left');
+    $('#arrow').css({ left: `${parseInt(left) + 10}px` })
 }
 
-window.onload = init;
+$('document').ready(function() {
+    // arrow
+    $('#arrow').css({ position: 'relative', left: '0px' });
+
+    // go to top
+    $('#go-to-top').click(function () {
+        $("html, body").animate({ scrollTop: "0px" });
+    });
+
+    // slide
+    $('h2').css('cursor', 'pointer');
+    $('h2').click(function () {
+        $(this).nextUntil('h2').slideToggle();
+    });
+
+    $('body').fadeIn(1250);
+ });
