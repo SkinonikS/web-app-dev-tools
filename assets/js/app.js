e458a7285c3d2c41c0e7455160d9543e0294e1f6
Vadim <skinnoniks@gmail.com>
2023-10-01 14:22:14 +0300

    Refactoring (#3)
    * Remove server.js
    
    * Remove socketio from index.html
    
    * Remove packages.json
    
    * Refactor app.js
    
    * Refactor index.html
    
    * Refactor app.css
    
    * Remove redundant script
    
    * Remove unused meta tags
    
    * Rename class names

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
-function moveRight() {
-    const left = $('#arrow').css('left');
-    $('#arrow').css({ left: `${parseInt(left) + 10}px` })
-}
+(function ($) {
+    $('document').ready(function() {
+        $('#arrow-img').css({ position: 'relative', left: '0px' });
+        
+        $('#arrow-btn').click(function () {
+            const $arrowImg = $('#arrow-img');
+        
+            $arrowImg.css({ left: `${parseInt($arrowImg.css('left')) + 10}px` })
+        });
+        
+        $('#go-to-top').click(function () {
+            $("html, body").animate({ scrollTop: "0px" });
+        });
 
-$('document').ready(function() {
-    // arrow
-    $('#arrow').css({ position: 'relative', left: '0px' });
-
-    // go to top
-    $('#go-to-top').click(function () {
-        $("html, body").animate({ scrollTop: "0px" });
-    });
-
-    // slide
-    $('h2').css('cursor', 'pointer');
-    $('h2').click(function () {
-        $(this).nextUntil('h2').slideToggle();
-    });
-
-    $('body').fadeIn(1250);
- });
+        $('.article__section')
+            .children('.article__section__title')
+            .css({ cursor: 'pointer' })
+            .click(function () {
+                $(this).next('.article__section__content').slideToggle();
+            });
+    
+        $('body').fadeIn(1250);
+     });
+})(jQuery);
